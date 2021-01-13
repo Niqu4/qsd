765098a66f3734ef7cbeb471d9f5ea4ece0728e9
Niqua <everybodyhatesnurali@gmail.com>
2021-01-13 12:33:11 +0600

    qsd final 4

--- config/cms.php
+++ config/cms.php
@@ -11,7 +11,7 @@ return [
     |
     */
 
-    'activeTheme' => 'demo',
+    'activeTheme' => 'zwiebl-zwiebl_stellar',
 
     /*
     |--------------------------------------------------------------------------
